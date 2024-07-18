6ebbba961cc00f47f312eed90bba14faf5a9fc4d
ziki1337 <artem_belokurov@inbox.ru>
2024-07-18 17:53:15 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
-
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  const {OnToggleButton, tg} = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
-  useEffect(() => {
-    tg.ready();
-  }, []);
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-    <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route index element={<ProductList />}></Route>
-        <Route path={'form'} element={<Form />}></Route>
-      </Routes>
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
